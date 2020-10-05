99ffdcfaaeae957c6748900085ad06e560e4c9af
ShindouMihou <kokocrunchsh@gmail.com>
2020-10-06 07:32:00 +0800

    Version 1.0.0: Contains only the basics, which is used to set the server count onto the website. Feel free to edit to your liking.
    Signed-off-by: ShindouMihou <kokocrunchsh@gmail.com>

--- src/main/java/tk/paradoxium/sbh/ShieldBotApi.java
+++ src/main/java/tk/paradoxium/sbh/ShieldBotApi.java
@@ -22,7 +22,7 @@ public class ShieldBotApi {
     /**
      * Used to create the Shield Bot API class.
      * @param token The token you receive from ShieldBotList.tk (not from Discord).
-     * @param botId Your bot client ID, can be found on developer portal on Discord (discordapp.com).
+     * @param botId Your bot client ID, can be found on developer portal on Discord (discordapp.com/developers).
      */
     public ShieldBotApi(String token, String botId) {
         this.url = new HttpUrl.Builder()
@@ -43,7 +43,7 @@ public class ShieldBotApi {
     /**
      * Used to create the Shield Bot API class.
      * @param token The token you receive from ShieldBotList.tk (not from Discord).
-     * @param botId Your bot client ID, can be found on developer portal on Discord (discordapp.com).
+     * @param botId Your bot client ID, can be found on developer portal on Discord (discordapp.com/developers).
      */
     public ShieldBotApi(String token, Long botId) {
         this.url = new HttpUrl.Builder()
